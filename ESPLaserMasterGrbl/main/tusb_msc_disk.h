a159cac49598e12f7ea50478238b17e4ca814871
ChengJunJie <1002667554@qq.com>
2021-06-04 21:22:34 +0800

    添加usb msc驱动

---
+++ ESPLaserMasterGrbl/main/tusb_msc_disk.h
@@ -0,0 +1,39 @@
+/*
+ * tusb_msc_disk.h
+ *
+ *  Created on: 2021年6月2日
+ *      Author: c
+ */
+
+#ifndef ESP_IDF_COMPONENTS_TINYUSB_ADDITIONS_INCLUDE_TUSB_MSC_DISK_H_
+#define ESP_IDF_COMPONENTS_TINYUSB_ADDITIONS_INCLUDE_TUSB_MSC_DISK_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#define CFG_EXAMPLE_MSC_READONLY
+
+
+void creat_UsbMscTask( void );
+
+
+
+
+
+
+
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+#endif /* ESP_IDF_COMPONENTS_TINYUSB_ADDITIONS_INCLUDE_TUSB_MSC_DISK_H_ */
