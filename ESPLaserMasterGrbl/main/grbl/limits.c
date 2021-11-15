0fa917b38194ce64540c326cb0ce583dc2cd39ed
Chengjunjie <1002667554@qq.com>
2021-11-15 12:40:18 +0800

    修复强制设置回零位置bug

--- ESPLaserMasterGrbl/main/grbl/limits.c
+++ ESPLaserMasterGrbl/main/grbl/limits.c
@@ -92,8 +92,8 @@ void limits_set_machine_positions (axes_signals_t cycle, bool add_pulloff)
     do {
         if (cycle.mask & bit(--idx)) {
         	sys.home_position[idx] = bit_istrue(settings.homing.dir_mask.value, bit(idx))
-        	                                      ? origin_offset[idx] + pulloff
-        	                                      : origin_offset[idx] - pulloff;
+        	                                      ? origin_offset[idx]
+        	                                      : origin_offset[idx];
 			sys_position[idx] = sys.home_position[idx] * settings.axis[idx].steps_per_mm;
         }
     } while(idx);
